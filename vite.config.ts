17d5597967e899d2289e2b6a6ca1cc63bbcf1078
tommycuong <tommycuong3130@gmail.com>
2025-02-15 22:47:59 +0700

    Fix Vercel deployment again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "dist", 
   },
   esbuild: {
     logOverride: { "this-is-undefined-in-esm": "silent" }
